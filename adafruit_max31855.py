d798d714facb994aecde5d176bbeea59a136ef91
tgikal <Kal_Fowle@yahoo.com>
2018-12-22 19:51:00 -0500

    Add files via upload
    Added linearized function.

--- adafruit_max31855.py
+++ adafruit_max31855.py
@@ -48,6 +48,8 @@ try:
 except ImportError:
     import ustruct as struct
 
+import math
+
 from adafruit_bus_device.spi_device import SPIDevice
 
 __version__ = "0.0.0-auto.0"
@@ -89,3 +91,78 @@ class MAX31855:
     def reference_temperature(self):
         """Internal reference temperature in degrees Celsius."""
         return self._read(True) * 0.625
+
+    @property
+    def linearized_temperature(self):
+        """Return the NIST-linearized thermocouple temperature value in degrees Celsius."""
+        # Read temperatures
+        probeTempC = self.temperature()
+        internalTempC = self.reference_temperature()
+        
+        # MAX31855 thermocouple voltage reading in mV
+        thermocoupleVoltage = (probeTempC - internalTempC) * 0.041276
+        
+        # MAX31855 cold junction voltage reading in mV
+        coldJunctionVoltage = (-0.176004136860E-01 +
+            0.389212049750E-01  * internalTempC +
+            0.185587700320E-04  * math.pow(internalTempC, 2.0) +
+            -0.994575928740E-07 * math.pow(internalTempC, 3.0) +
+            0.318409457190E-09  * math.pow(internalTempC, 4.0) +
+            -0.560728448890E-12 * math.pow(internalTempC, 5.0) +
+            0.560750590590E-15  * math.pow(internalTempC, 6.0) +
+            -0.320207200030E-18 * math.pow(internalTempC, 7.0) +
+            0.971511471520E-22  * math.pow(internalTempC, 8.0) +
+            -0.121047212750E-25 * math.pow(internalTempC, 9.0) +
+            0.118597600000E+00  * math.exp(-0.118343200000E-03 *
+            math.pow((internalTempC-0.126968600000E+03), 2.0)))
+
+        # cold junction voltage + thermocouple voltage
+        voltageSum = thermocoupleVoltage + coldJunctionVoltage
+
+        # calculate corrected temperature reading based on coefficients for 3 different ranges
+        # float b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10;
+        if voltageSum < 0:
+            b0 = 0.0000000E+00
+            b1 = 2.5173462E+01
+            b2 = -1.1662878E+00
+            b3 = -1.0833638E+00
+            b4 = -8.9773540E-01
+            b5 = -3.7342377E-01
+            b6 = -8.6632643E-02
+            b7 = -1.0450598E-02
+            b8 = -5.1920577E-04
+            b9 = 0.0000000E+00
+        elif voltageSum < 20.644:
+            b0 = 0.000000E+00
+            b1 = 2.508355E+01
+            b2 = 7.860106E-02
+            b3 = -2.503131E-01
+            b4 = 8.315270E-02
+            b5 = -1.228034E-02
+            b6 = 9.804036E-04
+            b7 = -4.413030E-05
+            b8 = 1.057734E-06
+            b9 = -1.052755E-08
+        elif voltageSum < 54.886:
+            b0 = -1.318058E+02
+            b1 = 4.830222E+01
+            b2 = -1.646031E+00
+            b3 = 5.464731E-02
+            b4 = -9.650715E-04
+            b5 = 8.802193E-06
+            b6 = -3.110810E-08
+            b7 = 0.000000E+00
+            b8 = 0.000000E+00
+            b9 = 0.000000E+00
+        else:
+            raise RuntimeError("temperature out of linearized range")
+        
+        return (b0 + b1 * voltageSum +
+            b2 * pow(voltageSum, 2.0) +
+            b3 * pow(voltageSum, 3.0) +
+            b4 * pow(voltageSum, 4.0) +
+            b5 * pow(voltageSum, 5.0) +
+            b6 * pow(voltageSum, 6.0) +
+            b7 * pow(voltageSum, 7.0) +
+            b8 * pow(voltageSum, 8.0) +
+            b9 * pow(voltageSum, 9.0))
